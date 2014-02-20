454ed221d676df795f4231c2cdf354d5d73da937
John Ryan <jcryan85@gmail.com>
2014-02-20 13:54:09 -0800

    initial ttt commit

--- tictactoe.py
+++ tictactoe.py
@@ -1,9 +1,5 @@
 
 print "Welcome to the tic tac toe"
-player = raw_input("Please select a player, (1): Player One, (2): Player Two")
-letter = raw_input("Please select (x): x or (o): o")
-location = int(raw_input("Where would you like to put the piece? (1-9) "))
-
 
 class Board(object):
 	def __init__(self):
@@ -15,14 +11,19 @@ class Board(object):
 		print "{0} - {1} - {2}".format(self.boardarray[6], self.boardarray[7], self.boardarray[8])
 
 	def addLetter(self, letter, location):
-		if self.boardarray[int(location-1)] != "x" or self.boardarray[int(location-1)] != "o":
-			self.boardarray[int(location-1)] = str(letter)
+		if self.boardarray[int(location)-1] != "x" or self.boardarray[int(location)-1] != "o":
+			self.boardarray[int(location)-1] = str(letter)
 
 newboard = Board()
+letter = ""
 while letter != "q":
+	letter = raw_input("Please select (x): x or (o): o ")
+	print letter
 	if letter == "x":
+		print "xxxxxxx"
 		location = raw_input("Where would you like to place the letter? (1-9) ")
 	elif letter == "o":
+		print "ooooooo"
 		location = raw_input("Where would you like to place the letter? (1-9) ")
 	else:
 		print "You messed up our game, jerk" 
@@ -30,8 +31,7 @@ while letter != "q":
 	newboard.addLetter(letter, location)
 	newboard.showBoard()
 
-	letter = raw_input("Please select (x): x or (o): o ")
-	location = int(raw_input("Where would you like to put the piece? (1-9) "))
+	
 
 
 
